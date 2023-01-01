f68eb284dccd21773eb22b9fe320318085458e51
Sivin <xiwenhec@163.com>
2023-01-01 19:41:00 +0800

    格式化部分代码

--- ffplay.c
+++ ffplay.c
@@ -1418,26 +1418,20 @@ static double get_master_clock(VideoState *is) {
 }
 
 static void check_external_clock_speed(VideoState *is) {
-  if (is->video_stream >= 0 &&
-              is->videoq.nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES ||
-      is->audio_stream >= 0 &&
-              is->audioq.nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES) {
-    set_clock_speed(&is->extclk,
-                    FFMAX(EXTERNAL_CLOCK_SPEED_MIN,
-                          is->extclk.speed - EXTERNAL_CLOCK_SPEED_STEP));
-  } else if ((is->video_stream < 0 ||
-              is->videoq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) &&
-             (is->audio_stream < 0 ||
-              is->audioq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES)) {
-    set_clock_speed(&is->extclk,
-                    FFMIN(EXTERNAL_CLOCK_SPEED_MAX,
-                          is->extclk.speed + EXTERNAL_CLOCK_SPEED_STEP));
+  if (is->video_stream >= 0 && is->videoq.nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES ||
+      is->audio_stream >= 0 && is->audioq.nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES) {
+        
+    set_clock_speed(&is->extclk, FFMAX(EXTERNAL_CLOCK_SPEED_MIN, is->extclk.speed - EXTERNAL_CLOCK_SPEED_STEP));
+
+  } else if ((is->video_stream < 0 || is->videoq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) &&
+             (is->audio_stream < 0 || is->audioq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES)) {
+    set_clock_speed(&is->extclk, FFMIN(EXTERNAL_CLOCK_SPEED_MAX, is->extclk.speed + EXTERNAL_CLOCK_SPEED_STEP));
+
   } else {
     double speed = is->extclk.speed;
-    if (speed != 1.0)
-      set_clock_speed(&is->extclk, speed + EXTERNAL_CLOCK_SPEED_STEP *
-                                                   (1.0 - speed) /
-                                                   fabs(1.0 - speed));
+    if (speed != 1.0) {
+      set_clock_speed(&is->extclk, speed + EXTERNAL_CLOCK_SPEED_STEP * (1.0 - speed) / fabs(1.0 - speed));
+    }
   }
 }
 
@@ -2470,7 +2464,7 @@ static void sdl_audio_callback(void *opaque, Uint8 *stream, int len) {
         }
         is->audio_buf_size = audio_size;
       }
-      //更新读索引缓存
+      //更新缓存读索引
       is->audio_buf_index = 0;
     }
 
